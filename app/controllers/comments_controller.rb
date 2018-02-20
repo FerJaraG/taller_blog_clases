1934315e96c186b3e0689ae2e63c651748428cbc
FerJaraG <mfjaragalleguillos@gmail.com>
2018-02-20 19:30:07 -0300

    blog

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+	before_action :set_resources, except: :create
+
+	def create
+		@comment = Comment.new(content: params[:comment][:content])
+		@post = Post.find(params[:post_id])
+		@comment.post = @post
+		@comment.save
+		redirect_to @post 
+	end
+
+	def destroy
+		@comment.destroy
+
+		redirect_to @post
+	end
+
+	def edit
+		
+	end
+
+	def update
+		@comment.update(content: params[:comment][:content])
+		redirect_to @post
+	end
+
+	private
+
+	def set_resources
+		@post = Post.find(params[:post_id])
+		@comment = Comment.find(params[:id])
+	end
+
+
+end
